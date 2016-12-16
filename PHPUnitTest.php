7265d11b654578b16a72bb8647b3591ec5e95646
cmdearcos <cmdearcos@gmail.com>
2016-12-16 22:46:24 +0100

    Class to test PHPUnit
    phpunit --verbose cmdearcos/utilities/PHPUnitTest

---
+++ PHPUnitTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of StackTest
+ *
+ * @author administrador
+ */
+class PHPUnitTest extends TestCase{
+    
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,15 @@ How to change directory permissions in ubuntu
 sudo setfacl -R -m u:www-data:rwX -m u:whoami:rwX [Directory] 
 sudo setfacl -dR -m u:www-data:rwX -m u:whoami:rwX [Directory]
 ``` 
+
+You can download it with this command
+``` 
+composer require cmdearcos/utilities dev-master
+``` 
+
+Install PHPUnit: [https://phpunit.de/manual/current/en/installation.html](https://phpunit.de/manual/current/en/installation.html)
+
+You can test PHPUnit running the next command:
+``` 
+phpunit --verbose cmdearcos/utilities/PHPUnitTest
+``` 
